014fb8eb9fe269cab307d33769ffb8f14760f09c
Alexandre Diaz Codina <alexandre.diaz.codina@gmail.com>
2021-09-29 21:02:53 +0200

    label encoding merger corrected and data types

--- API/fast_api_labanka.py
+++ API/fast_api_labanka.py
@@ -20,32 +20,32 @@ def index():
 
 
 @app.get("/predict")
-def predict(with_previous,
-            CNT_CHILDREN,               # 3
-            AMT_INCOME_TOTAL,           # 50000
-            DAYS_BIRTH,                 # 2530
-            DAYS_EMPLOYED,              # 270
-            ORGANIZATION_TYPE_TEXT,     # Electricity (Label encoding)
-            EXT_SOURCE2,                # floating € [0,1]
-            EXT_SOURCE3,                # floating € [0,1]
-            DEF_60_CNT_SOCIAL_CIRCLE,   # Default (integer number)
-            AMT_REQ_CREDIT_BUREAU_DAY,  # Default (integer number)
-            AMT_REQ_CREDIT_BUREAU_YEAR, # Default (integer number)
-            NAME_INCOME_TYPE,           # text, OHE
-            CURR_AMT_ANNUITY,           # Only for current application, floating 3500.9
-            CURR_AMT_CREDIT,            # Only for current applications floating 10000.5
-            OCCUPATION_TYPE,            # text OHE
-            NAME_CONTRACT_TYPE,         # if with_previous =1, value from params, if not default (OHE)
-            NAME_PORTFOLIO,             # if with_previous =1, value from params, if not default (OHE)
-            NAME_CONTRACT_STATUS,       # if with_previous =1, value from params, if not default (OHE)
-            AMT_ANNUITY,                # if with_previous =1, value from params, if not default 
-            AMT_CREDIT,                 # if with_previous =1, value from params, if not default 
-            APPLICATION_CREDIT_DIF,     # if with_previous =1, value from params, if not default 
-            PREV_REFUSED_COUNT,         # if with_previous =1, value from params, if not default 
-            PREV_APPROVED_COUNT,        # if with_previous =1, value from params, if not default 
-            AMT_PAYMENT,                # if with_previous =1, value from params, if not default (OHE)
-            LATE_PAY,                   # if with_previous =1, value from params, if not default (OHE)
-            AMT_MISS):
+def predict(with_previous: int,
+            CNT_CHILDREN: int,               # 3
+            AMT_INCOME_TOTAL: float,           # 50000
+            DAYS_BIRTH: int,                 # 2530
+            DAYS_EMPLOYED: int,              # 270
+            ORGANIZATION_TYPE_TEXT: str,     # Electricity (Label encoding)
+            EXT_SOURCE2: float,                # floating € [0,1]
+            EXT_SOURCE3: float,                # floating € [0,1]
+            DEF_60_CNT_SOCIAL_CIRCLE: int,   # Default (integer number)
+            AMT_REQ_CREDIT_BUREAU_DAY: int,  # Default (integer number)
+            AMT_REQ_CREDIT_BUREAU_YEAR: int, # Default (integer number)
+            NAME_INCOME_TYPE: str,           # text, OHE
+            CURR_AMT_ANNUITY: float,           # Only for current application, floating 3500.9
+            CURR_AMT_CREDIT: float,            # Only for current applications floating 10000.5
+            OCCUPATION_TYPE: str,            # text OHE
+            NAME_CONTRACT_TYPE: str,         # if with_previous =1, value from params, if not default (OHE)
+            NAME_PORTFOLIO: str,             # if with_previous =1, value from params, if not default (OHE)
+            NAME_CONTRACT_STATUS: str,       # if with_previous =1, value from params, if not default (OHE)
+            AMT_ANNUITY: float,                # if with_previous =1, value from params, if not default 
+            AMT_CREDIT: float,                 # if with_previous =1, value from params, if not default 
+            APPLICATION_CREDIT_DIF: float,     # if with_previous =1, value from params, if not default 
+            PREV_REFUSED_COUNT: int,         # if with_previous =1, value from params, if not default 
+            PREV_APPROVED_COUNT: int,        # if with_previous =1, value from params, if not default 
+            AMT_PAYMENT: float,                # if with_previous =1, value from params, if not default (OHE)
+            LATE_PAY: int,                   # if with_previous =1, value from params, if not default (OHE)
+            AMT_MISS: int):
     
 #Pending to calculate: ANNUITY_OVER_CREDIT_RATIO, CREDIT_TO_ANNUITY_RATIO (prev)
 
@@ -55,10 +55,10 @@ def predict(with_previous,
     else:
         ANNUITY_OVER_CREDIT_RATIO = int(CURR_AMT_ANNUITY) / int(CURR_AMT_CREDIT)
     
-    if int(CURR_AMT_ANNUITY) == 0 or  int(CURR_AMT_CREDIT) == 0:
+    if int(AMT_ANNUITY) == 0 or  int(AMT_CREDIT) == 0:
         CREDIT_TO_ANNUITY_RATIO = 0
     else:
-        CREDIT_TO_ANNUITY_RATIO = int(CURR_AMT_CREDIT) / int(CURR_AMT_ANNUITY)
+        CREDIT_TO_ANNUITY_RATIO = int(AMT_ANNUITY) / int(AMT_CREDIT)
 
     #Loading the master label encoder
 
@@ -193,11 +193,15 @@ def predict(with_previous,
             None
     
     ## Merging with the master label encoding df
-    clean_df = df.merge(master_label_encoder_df,on='ORGANIZATION_TYPE_TEXT',how='inner')
+    clean_df = df.merge(master_label_encoder_df,on='ORGANIZATION_TYPE_TEXT',how='left')
     clean_df.drop(columns='ORGANIZATION_TYPE_TEXT', inplace=True)
 
     # convert all columns of DataFrame
-    clean_df = clean_df.apply(pd.to_numeric) # convert all columns of DataFrame
+    cols = clean_df.select_dtypes(exclude=['float']).columns
+    clean_df[cols] = clean_df[cols].apply(pd.to_numeric, downcast='float', errors='coerce')
+    # Reordering columns in the right order as for the trained model
+
+    clean_df = clean_df[["CNT_CHILDREN","AMT_INCOME_TOTAL","DAYS_BIRTH","DAYS_EMPLOYED","ORGANIZATION_TYPE","EXT_SOURCE_2","EXT_SOURCE_3","DEF_60_CNT_SOCIAL_CIRCLE","AMT_REQ_CREDIT_BUREAU_DAY","AMT_REQ_CREDIT_BUREAU_YEAR","ANNUITY_OVER_CREDIT_RATIO","NAME_INCOME_TYPE_Businessman","NAME_INCOME_TYPE_Commercial associate","NAME_INCOME_TYPE_Maternity leave","NAME_INCOME_TYPE_Pensioner","NAME_INCOME_TYPE_State servant","NAME_INCOME_TYPE_Student","NAME_INCOME_TYPE_Unemployed","NAME_INCOME_TYPE_Working","NAME_INCOME_TYPE_nan","OCCUPATION_TYPE_Accountants","OCCUPATION_TYPE_Cleaning staff","OCCUPATION_TYPE_Cooking staff","OCCUPATION_TYPE_Core staff","OCCUPATION_TYPE_Drivers","OCCUPATION_TYPE_HR staff","OCCUPATION_TYPE_High skill tech staff","OCCUPATION_TYPE_IT staff","OCCUPATION_TYPE_Laborers","OCCUPATION_TYPE_Low-skill Laborers","OCCUPATION_TYPE_Managers","OCCUPATION_TYPE_Medicine staff","OCCUPATION_TYPE_Private service staff","OCCUPATION_TYPE_Realty agents","OCCUPATION_TYPE_Sales staff","OCCUPATION_TYPE_Secretaries","OCCUPATION_TYPE_Security staff","OCCUPATION_TYPE_Waiters/barmen staff","OCCUPATION_TYPE_nan","NAME_CONTRACT_TYPE_Cash loans_mean","NAME_CONTRACT_TYPE_Consumer loans_mean","NAME_CONTRACT_TYPE_POS_mean","NAME_CONTRACT_TYPE_Revolving loans_mean","NAME_PORTFOLIO_Cards_mean","NAME_PORTFOLIO_Cars_mean","NAME_PORTFOLIO_Cash_mean","NAME_PORTFOLIO_POS_mean","NAME_CONTRACT_STATUS_Approved_mean","NAME_CONTRACT_STATUS_Canceled_mean","NAME_CONTRACT_STATUS_Refused_mean","NAME_CONTRACT_STATUS_Unused offer_mean","AMT_ANNUITY_min","AMT_ANNUITY_max","AMT_ANNUITY_mean","CREDIT_TO_ANNUITY_RATIO_mean","CREDIT_TO_ANNUITY_RATIO_max","APPLICATION_CREDIT_DIFF_min","APPLICATION_CREDIT_DIFF_max","APPLICATION_CREDIT_DIFF_mean","PREV_REFUSED_COUNT_mean","PREV_APPROVED_COUNT_mean","AMT_PAYMENT","LATE_PAY","AMT_MISS","with_previous"]]
 
     # pipeline = get_model_from_gcp() OKEI
     model = get_joblib_gcp(bucket="wagon-data-618-le-banq", file_path='model/no_bureau_model_lgbm.sav')
