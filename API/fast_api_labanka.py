84ae2903f665d28f909fbc40853f0e7c7569a2c8
Alexandre Diaz Codina <alexandre.diaz.codina@gmail.com>
2021-10-03 15:03:16 +0200

    API corrected

--- API/fast_api_labanka.py
+++ API/fast_api_labanka.py
@@ -21,46 +21,53 @@ def index():
 
 
 @app.get("/predict")
-def predict(with_previous: int,
-            CNT_CHILDREN: int,               # 3
-            AMT_INCOME_TOTAL: float,           # 50000
-            DAYS_BIRTH: int,                 # 2530
-            DAYS_EMPLOYED: int,              # 270
-            ORGANIZATION_TYPE_TEXT: str,     # Electricity (Label encoding)
-            EXT_SOURCE2: float,                # floating € [0,1]
-            EXT_SOURCE3: float,                # floating € [0,1]
-            DEF_60_CNT_SOCIAL_CIRCLE: int,   # Default (integer number)
-            AMT_REQ_CREDIT_BUREAU_DAY: int,  # Default (integer number)
-            AMT_REQ_CREDIT_BUREAU_YEAR: int, # Default (integer number)
-            NAME_INCOME_TYPE: str,           # text, OHE
-            CURR_AMT_ANNUITY: float,           # Only for current application, floating 3500.9
-            CURR_AMT_CREDIT: float,            # Only for current applications floating 10000.5
-            OCCUPATION_TYPE: str,            # text OHE
-            NAME_CONTRACT_TYPE: str,         # if with_previous =1, value from params, if not default (OHE)
-            NAME_PORTFOLIO: str,             # if with_previous =1, value from params, if not default (OHE)
-            NAME_CONTRACT_STATUS: str,       # if with_previous =1, value from params, if not default (OHE)
-            AMT_ANNUITY: float,                # if with_previous =1, value from params, if not default 
-            AMT_CREDIT: float,                 # if with_previous =1, value from params, if not default 
-            APPLICATION_CREDIT_DIF: float,     # if with_previous =1, value from params, if not default 
-            PREV_REFUSED_COUNT: int,         # if with_previous =1, value from params, if not default 
-            PREV_APPROVED_COUNT: int,        # if with_previous =1, value from params, if not default 
-            AMT_PAYMENT: float,                # if with_previous =1, value from params, if not default (OHE)
-            LATE_PAY: int,                   # if with_previous =1, value from params, if not default (OHE)
-            AMT_MISS: int):
+def predict(with_previous: float,
+            CNT_CHILDREN: float,               
+            AMT_INCOME_TOTAL: float,           
+            DAYS_BIRTH: float,                 
+            DAYS_EMPLOYED: float,              
+            ORGANIZATION_TYPE_TEXT: str,     
+            EXT_SOURCE2: float,                
+            EXT_SOURCE3: float,               
+            DEF_60_CNT_SOCIAL_CIRCLE: float,   
+            AMT_REQ_CREDIT_BUREAU_DAY: float,  
+            AMT_REQ_CREDIT_BUREAU_YEAR: float, 
+            NAME_INCOME_TYPE: str,           
+            CURR_AMT_ANNUITY: float,           
+            CURR_AMT_CREDIT: float,            
+            OCCUPATION_TYPE: str,
+            NAME_CONTRACT_TYPE: str,
+            NAME_PORTFOLIO: str,
+            NAME_CONTRACT_STATUS: str,
+            AMT_ANNUITY_min: float,
+            AMT_ANNUITY_max: float,
+            AMT_ANNUITY_mean: float,
+            AMT_CREDIT: float,                 
+            APPLICATION_CREDIT_DIFF_min: float,
+            APPLICATION_CREDIT_DIFF_max: float,
+            APPLICATION_CREDIT_DIFF_mean: float,
+            PREV_REFUSED_COUNT: float,       
+            PREV_APPROVED_COUNT: float,       
+            AMT_PAYMENT: float,                
+            LATE_PAY: float,                   
+            AMT_MISS: float):
     
-#Pending to calculate: ANNUITY_OVER_CREDIT_RATIO, CREDIT_TO_ANNUITY_RATIO (prev)
-
-    # Feature engineering
+# Feature engineering
     if int(CURR_AMT_ANNUITY) == 0 or  int(CURR_AMT_CREDIT) == 0:
         ANNUITY_OVER_CREDIT_RATIO = 0
     else:
         ANNUITY_OVER_CREDIT_RATIO = int(CURR_AMT_ANNUITY) / int(CURR_AMT_CREDIT)
-    
-    if int(AMT_ANNUITY) == 0 or  int(AMT_CREDIT) == 0:
-        CREDIT_TO_ANNUITY_RATIO = 0
-    else:
-        CREDIT_TO_ANNUITY_RATIO = int(AMT_ANNUITY) / int(AMT_CREDIT)
 
+    if int(AMT_ANNUITY_mean) == 0 or  int(AMT_CREDIT) == 0:
+        CREDIT_TO_ANNUITY_RATIO_mean = 0
+    else:
+        CREDIT_TO_ANNUITY_RATIO_mean = int(AMT_CREDIT) / int(AMT_ANNUITY_mean)
+    
+    if int(AMT_ANNUITY_max) == 0 or  int(AMT_CREDIT) == 0:
+        CREDIT_TO_ANNUITY_RATIO_max = 0
+    else:
+        CREDIT_TO_ANNUITY_RATIO_max = int(AMT_CREDIT) / int(AMT_ANNUITY_max)
+    
     #Loading the master label encoder
 
     master_label_encoder_df = get_pickle_gcp(file_path='encoders/master_label_encoding.pkl')
@@ -107,48 +114,49 @@ def predict(with_previous: int,
     # Preparing the dictionary to append to the empty dataframe
     
     df_input_append = {'CNT_CHILDREN': CNT_CHILDREN, 'AMT_INCOME_TOTAL': AMT_INCOME_TOTAL, 'DAYS_BIRTH': DAYS_BIRTH,
-       'DAYS_EMPLOYED': DAYS_EMPLOYED, 'ORGANIZATION_TYPE_TEXT': ORGANIZATION_TYPE_TEXT,'EXT_SOURCE_2': EXT_SOURCE2, 'EXT_SOURCE_3': EXT_SOURCE3,
-       'DEF_60_CNT_SOCIAL_CIRCLE': DEF_60_CNT_SOCIAL_CIRCLE, 'AMT_REQ_CREDIT_BUREAU_DAY': AMT_REQ_CREDIT_BUREAU_DAY,
-       'AMT_REQ_CREDIT_BUREAU_YEAR': AMT_REQ_CREDIT_BUREAU_YEAR, 'ANNUITY_OVER_CREDIT_RATIO': ANNUITY_OVER_CREDIT_RATIO,
-       'NAME_INCOME_TYPE_Businessman':0, 'NAME_INCOME_TYPE_Commercial associate':0,
-       'NAME_INCOME_TYPE_Maternity leave':0, 'NAME_INCOME_TYPE_Pensioner':0,
-       'NAME_INCOME_TYPE_State servant':0, 'NAME_INCOME_TYPE_Student':0,
-       'NAME_INCOME_TYPE_Unemployed':0, 'NAME_INCOME_TYPE_Working':0,
-       'NAME_INCOME_TYPE_nan':0, 'OCCUPATION_TYPE_Accountants':0,
-       'OCCUPATION_TYPE_Cleaning staff':0, 'OCCUPATION_TYPE_Cooking staff':0,
-       'OCCUPATION_TYPE_Core staff':0, 'OCCUPATION_TYPE_Drivers':0,
-       'OCCUPATION_TYPE_HR staff':0, 'OCCUPATION_TYPE_High skill tech staff':0,
-       'OCCUPATION_TYPE_IT staff':0, 'OCCUPATION_TYPE_Laborers':0,
-       'OCCUPATION_TYPE_Low-skill Laborers':0, 'OCCUPATION_TYPE_Managers':0,
-       'OCCUPATION_TYPE_Medicine staff':0,
-       'OCCUPATION_TYPE_Private service staff':0,
-       'OCCUPATION_TYPE_Realty agents':0, 'OCCUPATION_TYPE_Sales staff':0,
-       'OCCUPATION_TYPE_Secretaries':0, 'OCCUPATION_TYPE_Security staff':0,
-       'OCCUPATION_TYPE_Waiters/barmen staff':0, 'OCCUPATION_TYPE_nan':0,
-       'NAME_CONTRACT_TYPE_Cash loans_mean':0,
-       'NAME_CONTRACT_TYPE_Consumer loans_mean':0, 'NAME_CONTRACT_TYPE_POS_mean':0,
-       'NAME_CONTRACT_TYPE_Revolving loans_mean':0, 'NAME_PORTFOLIO_Cards_mean':0,
-       'NAME_PORTFOLIO_Cars_mean':0, 'NAME_PORTFOLIO_Cash_mean':0,
-       'NAME_PORTFOLIO_POS_mean':0, 'NAME_CONTRACT_STATUS_Approved_mean':0,
-       'NAME_CONTRACT_STATUS_Canceled_mean':0,
-       'NAME_CONTRACT_STATUS_Refused_mean':0,
-       'NAME_CONTRACT_STATUS_Unused offer_mean': 0, 'AMT_ANNUITY_min': AMT_ANNUITY,
-       'AMT_ANNUITY_max': AMT_ANNUITY, 'AMT_ANNUITY_mean': AMT_ANNUITY, 'CREDIT_TO_ANNUITY_RATIO_mean': CREDIT_TO_ANNUITY_RATIO,
-       'CREDIT_TO_ANNUITY_RATIO_max': CREDIT_TO_ANNUITY_RATIO, 'APPLICATION_CREDIT_DIFF_min': APPLICATION_CREDIT_DIF,
-       'APPLICATION_CREDIT_DIFF_max': APPLICATION_CREDIT_DIF, 'APPLICATION_CREDIT_DIFF_mean': APPLICATION_CREDIT_DIF,
-       'PREV_REFUSED_COUNT_mean': PREV_REFUSED_COUNT, 'PREV_APPROVED_COUNT_mean': PREV_APPROVED_COUNT, 'AMT_PAYMENT': AMT_PAYMENT,
-       'LATE_PAY': LATE_PAY, 'AMT_MISS': AMT_MISS,'with_previous': with_previous}
+    'DAYS_EMPLOYED': DAYS_EMPLOYED, 'ORGANIZATION_TYPE_TEXT': ORGANIZATION_TYPE_TEXT,'EXT_SOURCE_2': EXT_SOURCE2, 'EXT_SOURCE_3': EXT_SOURCE3,
+    'DEF_60_CNT_SOCIAL_CIRCLE': DEF_60_CNT_SOCIAL_CIRCLE, 'AMT_REQ_CREDIT_BUREAU_DAY': AMT_REQ_CREDIT_BUREAU_DAY,
+    'AMT_REQ_CREDIT_BUREAU_YEAR': AMT_REQ_CREDIT_BUREAU_YEAR, 'ANNUITY_OVER_CREDIT_RATIO': ANNUITY_OVER_CREDIT_RATIO,
+    'NAME_INCOME_TYPE_Businessman':0, 'NAME_INCOME_TYPE_Commercial associate':0,
+    'NAME_INCOME_TYPE_Maternity leave':0, 'NAME_INCOME_TYPE_Pensioner':0,
+    'NAME_INCOME_TYPE_State servant':0, 'NAME_INCOME_TYPE_Student':0,
+    'NAME_INCOME_TYPE_Unemployed':0, 'NAME_INCOME_TYPE_Working':0,
+    'NAME_INCOME_TYPE_nan':0, 'OCCUPATION_TYPE_Accountants':0,
+    'OCCUPATION_TYPE_Cleaning staff':0, 'OCCUPATION_TYPE_Cooking staff':0,
+    'OCCUPATION_TYPE_Core staff':0, 'OCCUPATION_TYPE_Drivers':0,
+    'OCCUPATION_TYPE_HR staff':0, 'OCCUPATION_TYPE_High skill tech staff':0,
+    'OCCUPATION_TYPE_IT staff':0, 'OCCUPATION_TYPE_Laborers':0,
+    'OCCUPATION_TYPE_Low-skill Laborers':0, 'OCCUPATION_TYPE_Managers':0,
+    'OCCUPATION_TYPE_Medicine staff':0,
+    'OCCUPATION_TYPE_Private service staff':0,
+    'OCCUPATION_TYPE_Realty agents':0, 'OCCUPATION_TYPE_Sales staff':0,
+    'OCCUPATION_TYPE_Secretaries':0, 'OCCUPATION_TYPE_Security staff':0,
+    'OCCUPATION_TYPE_Waiters/barmen staff':0, 'OCCUPATION_TYPE_nan':0,
+    'NAME_CONTRACT_TYPE_Cash loans_mean':0,
+    'NAME_CONTRACT_TYPE_Consumer loans_mean':0, 'NAME_CONTRACT_TYPE_POS_mean':0,
+    'NAME_CONTRACT_TYPE_Revolving loans_mean':0, 'NAME_PORTFOLIO_Cards_mean':0,
+    'NAME_PORTFOLIO_Cars_mean':0, 'NAME_PORTFOLIO_Cash_mean':0,
+    'NAME_PORTFOLIO_POS_mean':0, 'NAME_CONTRACT_STATUS_Approved_mean':0,
+    'NAME_CONTRACT_STATUS_Canceled_mean':0,
+    'NAME_CONTRACT_STATUS_Refused_mean':0,
+    'NAME_CONTRACT_STATUS_Unused offer_mean': 0, 'AMT_ANNUITY_min': AMT_ANNUITY_min,
+    'AMT_ANNUITY_max': AMT_ANNUITY_max, 'AMT_ANNUITY_mean': AMT_ANNUITY_mean, 'CREDIT_TO_ANNUITY_RATIO_mean': CREDIT_TO_ANNUITY_RATIO_mean,
+    'CREDIT_TO_ANNUITY_RATIO_max': CREDIT_TO_ANNUITY_RATIO_max, 'APPLICATION_CREDIT_DIFF_min': APPLICATION_CREDIT_DIFF_min,
+    'APPLICATION_CREDIT_DIFF_max': APPLICATION_CREDIT_DIFF_max, 'APPLICATION_CREDIT_DIFF_mean': APPLICATION_CREDIT_DIFF_mean,
+    'PREV_REFUSED_COUNT_mean': PREV_REFUSED_COUNT, 'PREV_APPROVED_COUNT_mean': PREV_APPROVED_COUNT, 'AMT_PAYMENT': AMT_PAYMENT,
+    'LATE_PAY': LATE_PAY, 'AMT_MISS': AMT_MISS,'with_previous': with_previous}
 
     df = input_df.append(df_input_append, ignore_index = True)
 
     # We will now iterate over each of the OHE elements and assign 1 to the one that matches clients input
 
-    ## Name Income Type
+   ## Name Income Type
 
     name_income_type_cols = [col for col in df.columns if 'NAME_INCOME_TYPE_' in col]
 
     for i in name_income_type_cols:
-        if NAME_INCOME_TYPE in i: 
+        after_strip = i.replace("NAME_INCOME_TYPE_","")
+        if NAME_INCOME_TYPE == after_strip: 
             df[i]=1
         else: 
             None
@@ -158,7 +166,8 @@ def predict(with_previous: int,
     occupation_type_cols = [col for col in df.columns if 'OCCUPATION_TYPE_' in col]
 
     for i in occupation_type_cols:
-        if OCCUPATION_TYPE in i: 
+        after_strip = i.replace("OCCUPATION_TYPE_","")
+        if OCCUPATION_TYPE == after_strip: 
             df[i]=1
         else: 
             None
@@ -198,10 +207,9 @@ def predict(with_previous: int,
     clean_df.drop(columns='ORGANIZATION_TYPE_TEXT', inplace=True)
 
     # convert all columns of DataFrame
-    cols = clean_df.select_dtypes(exclude=['float']).columns
-    clean_df[cols] = clean_df[cols].apply(pd.to_numeric, downcast='float', errors='coerce')
+    clean_df = clean_df.apply(pd.to_numeric)
+    
     # Reordering columns in the right order as for the trained model
-
     clean_df = clean_df[["CNT_CHILDREN","AMT_INCOME_TOTAL","DAYS_BIRTH","DAYS_EMPLOYED","ORGANIZATION_TYPE","EXT_SOURCE_2","EXT_SOURCE_3","DEF_60_CNT_SOCIAL_CIRCLE","AMT_REQ_CREDIT_BUREAU_DAY","AMT_REQ_CREDIT_BUREAU_YEAR","ANNUITY_OVER_CREDIT_RATIO","NAME_INCOME_TYPE_Businessman","NAME_INCOME_TYPE_Commercial associate","NAME_INCOME_TYPE_Maternity leave","NAME_INCOME_TYPE_Pensioner","NAME_INCOME_TYPE_State servant","NAME_INCOME_TYPE_Student","NAME_INCOME_TYPE_Unemployed","NAME_INCOME_TYPE_Working","NAME_INCOME_TYPE_nan","OCCUPATION_TYPE_Accountants","OCCUPATION_TYPE_Cleaning staff","OCCUPATION_TYPE_Cooking staff","OCCUPATION_TYPE_Core staff","OCCUPATION_TYPE_Drivers","OCCUPATION_TYPE_HR staff","OCCUPATION_TYPE_High skill tech staff","OCCUPATION_TYPE_IT staff","OCCUPATION_TYPE_Laborers","OCCUPATION_TYPE_Low-skill Laborers","OCCUPATION_TYPE_Managers","OCCUPATION_TYPE_Medicine staff","OCCUPATION_TYPE_Private service staff","OCCUPATION_TYPE_Realty agents","OCCUPATION_TYPE_Sales staff","OCCUPATION_TYPE_Secretaries","OCCUPATION_TYPE_Security staff","OCCUPATION_TYPE_Waiters/barmen staff","OCCUPATION_TYPE_nan","NAME_CONTRACT_TYPE_Cash loans_mean","NAME_CONTRACT_TYPE_Consumer loans_mean","NAME_CONTRACT_TYPE_POS_mean","NAME_CONTRACT_TYPE_Revolving loans_mean","NAME_PORTFOLIO_Cards_mean","NAME_PORTFOLIO_Cars_mean","NAME_PORTFOLIO_Cash_mean","NAME_PORTFOLIO_POS_mean","NAME_CONTRACT_STATUS_Approved_mean","NAME_CONTRACT_STATUS_Canceled_mean","NAME_CONTRACT_STATUS_Refused_mean","NAME_CONTRACT_STATUS_Unused offer_mean","AMT_ANNUITY_min","AMT_ANNUITY_max","AMT_ANNUITY_mean","CREDIT_TO_ANNUITY_RATIO_mean","CREDIT_TO_ANNUITY_RATIO_max","APPLICATION_CREDIT_DIFF_min","APPLICATION_CREDIT_DIFF_max","APPLICATION_CREDIT_DIFF_mean","PREV_REFUSED_COUNT_mean","PREV_APPROVED_COUNT_mean","AMT_PAYMENT","LATE_PAY","AMT_MISS","with_previous"]]
 
     # pipeline = get_model_from_gcp() OKEI
